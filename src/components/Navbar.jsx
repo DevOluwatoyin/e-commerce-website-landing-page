a5ff4e91d2c327894302c15daa96a5e577e37b03
Oluwatoyin <oluwatoyinjolaoso001@gmail>
2024-01-26 14:30:15 +0100

    working on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,47 +1,58 @@
+// Import necessary modules
 "use client";
 
 import React from "react";
 import Image from "next/image";
 import logo from "/public/logo.svg";
 import ham from "/public/icons/ham.svg";
-import { usePathname } from "next/navigation";
 import close from "/public/icons/close.svg";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 
+// Navbar component
 const Navbar = () => {
   const pathname = usePathname();
 
   return (
-    <header className="p-3">
-      <Image src={logo} alt="Greenmind Logo" priority />
-      <Image src={ham} alt="open Icon" priority className="md:hidden" />
-      <Image src={close} alt="Close Icon" priority className="md:hidden" />
+    <header className="fixed top-0 left-0 right-0 z-50 bg-white shadow-lg p-3 md:flex md:items-center md:justify-between">
+      {/* Logo */}
+      <div className="flex items-center">
+        <Image src={logo} alt="Greenmind Logo" priority />
+        <Image src={ham} alt="Open Icon" priority className="md:hidden ml-4" />
+        <Image
+          src={close}
+          alt="Close Icon"
+          priority
+          className="md:hidden ml-4"
+        />
+      </div>
 
-      <nav>
-        <ul>
+      {/* Navbar Links */}
+      <nav className="md:flex hidden">
+        <ul className="flex space-x-4">
           <Link
+            href="/"
             className={`link ${
               pathname === "/" ? "font-bold underline underline-offset-2" : ""
             }`}
-            href="/"
           >
             Home
           </Link>
           <Link
-            className={`link ${pathname === "/#about" ? "active" : ""}`}
             href="#about"
+            className={`link ${pathname === "/#about" ? "active" : ""}`}
           >
             About
           </Link>
           <Link
-            className={`link ${pathname === "/#categories" ? "active" : ""}`}
             href="#categories"
+            className={`link ${pathname === "/#categories" ? "active" : ""}`}
           >
             Categories
           </Link>
           <Link
-            className={`link ${pathname === "/products" ? "active" : ""}`}
             href="/products"
+            className={`link ${pathname === "/products" ? "active" : ""}`}
           >
             Products
           </Link>
