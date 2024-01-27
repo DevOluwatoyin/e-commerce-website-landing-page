9256ad6077882242a55bf87293bd16eb2ddcfd65
Oluwatoyin <oluwatoyinjolaoso001@gmail>
2024-01-27 12:29:38 +0100

    completed best selling section

--- src/sections/BestSelling.jsx
+++ src/sections/BestSelling.jsx
@@ -6,21 +6,19 @@ import { allPlants } from "@/constants/plants";
 const BestSelling = () => {
   const plants = allPlants.slice(0, 3);
   return (
-    <section className="flex flex-col py-10 px-8 md:flex-row text-dark-two justify-around">
-      <div className="space-y-4">
+    <section className="flex flex-col my-20 px-8 text-dark-two justify-around gap-10 max-w-[1300px] mx-auto md:flex-row">
+      <div className="space-y-4 text-center md:text-left">
         <h3 className="text-4xl font-bold">Best Selling Plants</h3>
         <p className="text-lg text-gray-light">
           Easiest way to healthy life by buying your favorite plants{" "}
         </p>
-        <Button text="See more" bgColor="#C1DCDC" maxWidth="168px" />
+        <Button text="See more" bgColor="#C1DCDC" maxWidth="175px" />
       </div>
-      <div className="flex justify-between flex-col gap-10 md:flex-row">
+      <div className="flex justify-between items-center flex-wrap flex-col gap-10 md:flex-row">
         {plants.map((plant, id) => (
-          <div key={id} className="space-y-4 text-lg font-medium">
-            <div className="max-w-[300px]">
-              <Image src={plant.image} alt={plant.name} className="w-[300px]" />
-            </div>
-            <p>{plant.name}</p>
+          <div key={id} className="text-lg font-medium flex-1 text-center md:text-left group">
+            <Image src={plant.image} alt={plant.name} className="group-hover:opacity-[0.7] cursor-pointer transition-opacity" />
+            <p className="mt-3 mb-1">{plant.name}</p>
             <p className="text-gray-light">{plant.price}</p>
           </div>
         ))}
